a07409c78f086d5366966623146e07eba8743210
Banjo3601 <paolo30500@gmail.com>
2023-01-06 17:05:52 +0100

    Première version

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            {/*Les images importées depuis la  balise IMG 
+            sont accessibles dans "public"*/}
+            <img src="/logo192.png" alt="logo react"/>
+            <h2>React World</h2>
+        </div>
+    );
+};
+
+export default Logo;
